de65679aec630f07deb844e956ecc6f613823249
orlyanSKY <000slim001@gmail.com>
2023-01-12 00:43:21 +0300

    add camera

--- Screens/CreatePostsScreen.js
+++ Screens/CreatePostsScreen.js
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { Camera, CameraType } from "expo-camera";
 const CreatePostsScreen = () => {
   return (
-    <View>
-      <Text>CreatePostsScreen</Text>
+    <View style={styles.container}>
+      <Camera style={styles.camera}>
+        <TouchableOpacity style={styles.snapshot}>
+          <Feather name="camera" size={24} color="#BDBDBD" />
+        </TouchableOpacity>
+      </Camera>
     </View>
   );
 };
 
 export default CreatePostsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  camera: {
+    height: 240,
+    marginTop: 32,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "#BDBDBD",
+    borderRadius: 8,
+  },
+  snapshot: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    borderWidth: 1,
+    borderColor: "#BDBDBD",
+  },
+});
