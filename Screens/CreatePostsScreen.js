c29a6f951d7c188231b07b743cacbad50d05fa1f
orlyanSKY <000slim001@gmail.com>
2023-01-13 12:49:32 +0300

    add front/back camera usage

--- Screens/CreatePostsScreen.js
+++ Screens/CreatePostsScreen.js
@@ -1,12 +1,42 @@
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { Feather } from "@expo/vector-icons";
 import { Camera, CameraType } from "expo-camera";
+import { Feather } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+
 const CreatePostsScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(CameraType.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera}>
+      <Camera style={styles.camera} type={type}>
         <TouchableOpacity style={styles.snapshot}>
           <Feather name="camera" size={24} color="#BDBDBD" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setType(
+              type === CameraType.back ? CameraType.front : CameraType.back
+            );
+          }}
+        >
+          <MaterialIcons name="flip-camera-android" size={24} color="#BDBDBD" />
         </TouchableOpacity>
       </Camera>
     </View>
@@ -18,17 +48,15 @@ export default CreatePostsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingLeft: 16,
-    paddingRight: 16,
   },
+
   camera: {
     height: 240,
+    marginHorizontal: 16,
     marginTop: 32,
     alignItems: "center",
     justifyContent: "center",
-    borderWidth: 1,
-    borderColor: "#BDBDBD",
-    borderRadius: 8,
+    borderRadius: 10,
   },
   snapshot: {
     alignItems: "center",
@@ -39,5 +67,10 @@ const styles = StyleSheet.create({
     height: 60,
     borderWidth: 1,
     borderColor: "#BDBDBD",
+  },
+  button: {
+    position: "absolute",
+    right: 10,
+    bottom: 10,
   },
 });
